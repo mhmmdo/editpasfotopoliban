c521913c1618d9248673949f3643b3bb301895f3
Muhammad Ridho <144815479+mhmmdo@users.noreply.github.com>
2023-10-08 10:31:17 +0800

    Update author.ts

--- data/author.ts
+++ data/author.ts
@@ -1,3 +1,3 @@
-export const AUTHOR_NAME: string = "Abiel Zulio M"
-export const AUTHOR_URL: string = "https://twitter.com/abielzulio"
+export const AUTHOR_NAME: string = "Muhammad Ridho"
+export const AUTHOR_URL: string = "https://instagram.com/drlanga_"
 export const AUTHOR_IMG_URL: string = "/img/author.jpg"
